323cc7da08e10381a6f4b04528fa060d6a409c32
Zach Levine <zblevine@gmail.com>
2019-08-01 20:37:52 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const { Component } = React;
+class App extends React.Component {
+    constructor() {
+        super();
+        const startNumber = 20;
+        const users = [];
+        for (let i = 0; i < startNumber; i++) {
+            users.push(faker.fake('{{name.firstName}} {{name.lastName}} \n {{internet.userName}}'));
+        }
+        this.state = { users };
+    }
+    render() {
+        const { users } = this.state;
+        return app;
+    }
+}
