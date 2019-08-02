54fe95c5b06bd8fa3af8d1f80db53f64790e10ab
Zach Levine <zblevine@gmail.com>
2019-08-01 21:34:44 -0400

    much of functionality

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
 const root = document.querySelector('#root');
 
 const startNumber = 20;
-
-
+const generateUser = (fav) => {
+    return {
+        text: faker.fake('{{name.firstName}} {{name.lastName}} {{internet.userName}}'),
+        favorite: fav
+    }
+}
 
 class App extends React.Component {
     constructor() {
         super();
-        const generateUsers = () => {
-            return faker.fake('{{name.firstName}} {{name.lastName}} \n {{internet.userName}}');
+
+        const users = [];
+        for (let i = 0; i < startNumber; i++) {
+            let bool = Math.random() > 0.5;
+            users.push(generateUser(bool));
         }
-
-        // const users = [];
-        // for (let i = 0; i < startNumber; i++) {
-        //     users.push(faker.fake('{{name.firstName}} {{name.lastName}} \n {{internet.userName}}'));
-        // }
-        this.state = { user};
-
+        this.state = { users };
     }
-
-
-
+    favoriteCount() {
+        return this.state.users.reduce((acc, user) => (user.favorite ? acc + 1 : acc), 0);
+    }
     render() {
-        const {user} = this.state;
-        const fakeUser = React.createElement('div',{key: user}, user);
-        return fakeUser;
+        const { users } = this.state;
+        const toggleFavorite = (ev) => {
+            ev.target.classList.toggle('favorite');
+        }
+        const addUser = () => {
+            this.state.users.push(generateUser(true));
+            this.setState({users});
+        }
+        const linkText = `You have ${this.favoriteCount()} favorite users!`;
+        const addBtn = React.createElement('div', {onClick: addUser}, linkText);
+        const lis = users.map( (user, idx) => {
+            const styleText = user.favorite ? 'favorite' : '';
+            return React.createElement('li', {key: idx, className: styleText, onClick: toggleFavorite}, user.text);
+        });
+        const app = React.createElement('ul', null, addBtn, lis);
+        return app;
     }
 }
 
