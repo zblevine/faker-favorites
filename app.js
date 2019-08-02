bf3243b1e39b8914cc6603f6f72b6f6e7af68892
Effy Zhang <effy@squareup.com>
2019-08-01 21:06:44 -0400

    add user

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const root = document.querySelector('#root');
 
+const startNumber = 20;
+
+
+
 class App extends React.Component {
     constructor() {
         super();
-        const startNumber = 20;
-        const users = [];
-        for (let i = 0; i < startNumber; i++) {
-            users.push(faker.fake('{{name.firstName}} {{name.lastName}} \n {{internet.userName}}'));
+        const generateUsers = () => {
+            return faker.fake('{{name.firstName}} {{name.lastName}} \n {{internet.userName}}');
         }
-        this.state = { users };
+
+        // const users = [];
+        // for (let i = 0; i < startNumber; i++) {
+        //     users.push(faker.fake('{{name.firstName}} {{name.lastName}} \n {{internet.userName}}'));
+        // }
+        this.state = { user};
+
     }
+
+
+
     render() {
-        const { users } = this.state;
-        return app;
+        const {user} = this.state;
+        const fakeUser = React.createElement('div',{key: user}, user);
+        return fakeUser;
     }
 }
+
+ReactDOM.render(React.createElement(App), root);
